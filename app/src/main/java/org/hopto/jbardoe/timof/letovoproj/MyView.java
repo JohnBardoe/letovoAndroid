3964b253f466abca37d10ddc2b4a317fb18a6957
JohnBardoe <yta1@list.ru>
2019-01-28 17:02:33 +0300

    Initial commit

---
+++ app/src/main/java/org/hopto/jbardoe/timof/letovoproj/MyView.java
@@ -0,0 +1,55 @@
+package org.hopto.jbardoe.timof.letovoproj;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.drawable.Drawable;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.Random;
+
+public class MyView extends View {
+
+    private Paint mPaint;
+    private Path mPath;
+
+    public MyView(Context context) {
+        super(context);
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(10);
+        mPath = new Path();
+    }
+
+    public void onDraw(Canvas canvas){
+        canvas.drawPath(mPath, mPaint);
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()){
+
+            case MotionEvent.ACTION_DOWN:
+                mPath.moveTo(event.getX(), event.getY());
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(event.getX(), event.getY());
+                invalidate();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+
+        return true;
+    }
+}
